902f3b98b6099e057949b17d3256849efb7c73bc
SleepyLazarus <truxtonql@gmail.com>
2020-08-17 16:01:23 -0400

    Tasks 1-3 done, let's hope I can work through 4.

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ *  Counter1 is using local variables where counter uses global.
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ *  Counter 1 uses a closure, you can tell because the variables are stored inside the function. 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * Counter 1 is best used for an application pertaining to itself, counter 2 would be good for using the data for other uses across the application. 
 */
 
 // counter1 code
@@ -56,11 +56,14 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+  let min = Math.ceil(0);
+  let max = Math.floor(2);
+  let teamScore = Math.random() * (max - min + 1) + min;
+  return Math.round(teamScore);
 }
+
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -76,11 +79,16 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inninga, inningh, ammount){
+  const gameScore = {
+    Away: (inninga * ammount),
+    Home: (inningh * ammount),
+  }
 
-  /*Code Here*/
-
+  return gameScore;
 }
+
+console.log(finalScore(inning(),inning(), 9));
 
 /* Task 4: 
 
